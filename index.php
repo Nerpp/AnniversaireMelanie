eae882f7a368e6729f06c1a963c528169d904d9c
Nerpp <regiaurelien@gmail.com>
2019-04-05 21:11:37 +0200

    Debut du projet

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+header($_SERVER["SERVER_PROTOCOL"] . " 404 Not Found");
+header('Content-Type: text/html; charset=utf-8');
---
+++ vue/accueil.vue.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Billet simple pour l'Alaska</title>
+    <link rel="stylesheet" href="public/css/style.accueil.css" />
+</head>
+
+<body>
+    <img div="imageLully" src="public/image/Lully.jpg" alt="Lully_Mexicain">
+
+</body>
+
+</html>
