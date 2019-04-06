b83edaa54b1a5eae042ff5f4c4912825fbfe89f7
Nerpp <regiaurelien@gmail.com>
2019-04-06 13:56:37 +0200

    Ajout du fichier Css a reponse

--- vue/reponse.vue.php
+++ vue/reponse.vue.php
@@ -1,3 +1,20 @@
-<?php
+<html>
 
-var_dump('je suis sur la page reponse');
+<head>
+    <meta charset="UTF-8">
+    <title>Lully La Mafia</title>
+    <!-- admin mobile -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- appel au style css -->
+    <link rel="stylesheet" href="public/css/style.bonneReponse.css" />
+    <!-- bootstrapcdn -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <h1 class="titre">Bonne Reponse</h1>
+
+</body>
+
+</html>
